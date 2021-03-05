2d16695bb1503eca67aa8e456be54524770a7224
williamlin0518 <80030403+williamlin0518@users.noreply.github.com>
2021-03-05 16:11:06 +0800

    Add files via upload

---
+++ first.java
@@ -0,0 +1,12 @@
+public class first
+{
+    public static void main(String[]  args)
+    {
+        String asdf="fefefef";
+        int income=100_000;
+
+        if (income>100000)
+            asdf="first";
+        System.out.println(asdf);
+    }
+}
