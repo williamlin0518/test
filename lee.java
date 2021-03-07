b66649866b947d4ccb2c9851b250f7919723ae7e
williamlin0518 <illiamw81@gmail.com>
2021-03-07 16:20:10 +0800

    lee

---
+++ lee.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class lee {
+
+    public static void main(String[] idontknowwahtisit){
+        String message ="Hello World"+"!!";
+        System.out.println(message.replace("H","L"));
+        System.out.print(message);//immutable
+        System.out.println(message.trim());
+
+        int[] numbers={1,3,2,4,5};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(numbers.length);
+    }
+    
+}
